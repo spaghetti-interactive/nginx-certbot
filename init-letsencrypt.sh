96d8973603cae9e8460044a2e0df2d8232b587ac
Franjanko <gtodini@gmail.com>
2023-12-15 16:53:43 +0100

    added site

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 #domains=(example.org www.example.org)
 # domains=(solitaireplus.net www.solitaireplus.net)
-domains=(raminopiu.it www.raminopiu.it)
+domains=(escobamas.es www.escobamas.es)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="giochipiu@gmail.com" # Adding a valid address is strongly recommended
