660c69e579ecd0a0a7a133348a5621f99818829a
Franjanko <gtodini@gmail.com>
2024-01-17 11:36:52 +0100

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -33,7 +33,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+NGINX_FOLDER="$nginx_srv_dockprom" docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -46,7 +46,7 @@ NGINX_FOLDER="$nginx_srv_dockprom" docker-compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+NGINX_FOLDER="$nginx_srv_dockprom" docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -69,7 +69,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+NGINX_FOLDER="$nginx_srv_dockprom" docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -80,4 +80,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+NGINX_FOLDER="$nginx_srv_dockprom" docker-compose exec nginx nginx -s reload
