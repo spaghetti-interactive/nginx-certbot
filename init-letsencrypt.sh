29e6fe56a8bca46667a74823b3175831c6614c9d
Franjanko <gtodini@gmail.com>
2024-01-17 10:41:46 +0100

    added grafana.giochiopiu.it

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 #domains=(example.org www.example.org)
 # domains=(solitaireplus.net www.solitaireplus.net)
-domains=(checkersplus.com www.checkersplus.com)
+domains=(grafana.giochipiu.it)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="giochipiu@gmail.com" # Adding a valid address is strongly recommended
