bfc1d79b975826c84eeaa071ae68dc74e99d9391
Franjanko <gtodini@gmail.com>
2024-01-17 11:34:12 +0100

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,6 +12,7 @@ rsa_key_size=4096
 data_path="./data/certbot"
 email="giochipiu@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+nginx_folder="nginx_srv_dockprom" #folder containing websites conf
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -41,7 +42,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+NGINX_FOLDER="$nginx_srv_dockprom" docker-compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
